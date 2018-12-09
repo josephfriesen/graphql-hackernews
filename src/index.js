021774714e8ff6fd4500b6be7bc8b60a2b12f9e6
Joe Friesen <friesen.josephc@gmail.com>
2018-12-09 15:17:09 -0800

    Complete through tutorial lesson "Authentication"

--- src/index.js
+++ src/index.js
@@ -1,64 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
-const v4 = require('uuid/v4');
+const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
 
-let links = [
-  {
-    "id": "1",
-    "url": "https://www.website.cool",
-    "description": "a hot link"
-  },
-  {
-    "id": "2",
-    "url": "https://www.howtographql.com",
-    "description": "a webbed site"
-  }
-];
-
-let idCount = links.length
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => {
-      const theLinkIAmLookingFor = links.find(link => link.id === args.id);
-      return theLinkIAmLookingFor;
-    }
-  },
-  Mutation: {
-    post: (root, args) => {
-       const link = {
-        id: v4(),
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link // return link as specified in schema Mutation.post(): Link!
-    },
-
-    updateLink: (root, args) => {
-      const newLink = {id: args.id, url: args.url, description: args.description};
-      links.forEach(link => {
-        if (link.id === newLink.id) {
-          link.id = args.id;
-          link.url = args.url;
-          link.description = args.description;
-        }
-      });
-      return newLink;
-    },
-
-    deleteLink: (root, args) => {
-      const badLinkIndex = links.findIndex(item => item.id === args.id);
-      const badLink = links[badLinkIndex];
-      links.splice(badLinkIndex,1);
-      console.log(links);
-      return badLink;
-    }
-  }
+  Query,
+  Mutation,
+  AuthPayload
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/joe-friesen-2baea5/graphql-hackernews/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 })
 server.start(() => console.log('Server is running on http://localhost:4000'));
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context) // getUserId retrieves the user ID stored in the JWT that is set at the Authorization header of the incoming HTTP request
+  return context.db.mutation.createLink(
+    {
+      data: {
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } }, // connect the new Link with the User that created it using the connect mutation
+      },
+    },
+    info,
+  )
+}
+
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10) // encrypt password using bcryptjs library
+  const user = await context.db.mutation.createUser({ // save user to database
+    data: { ...args, password },
+  }, `{ id }`)
+  const token = jwt.sign({ userId: user.id }, APP_SECRET) // generate a JWT, signed with APP_SECRET
+
+  return {
+    token,
+    user,
+  }
+}
+
+async function login(parent, args, context, info) {
+  const user = await context.db.query.user({ where: { email: args.email } }, ` { id password } `) // Get existing user info by specified email address. If no user found, throw error. If user found, return id and encrypted password
+  if (!user) {
+    throw new Error('No such user found')
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password) // compare provided password with stored password
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+}
