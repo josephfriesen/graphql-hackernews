6a51565914efee4cd8c443c43548e8744772a397
Joe Friesen <friesen.josephc@gmail.com>
2018-12-09 21:28:43 -0800

    Starting React/Apollo tutorial; move previous work to '/server/'.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { Prisma } = require('prisma-binding');
-const Query = require('./resolvers/Query');
-const Mutation = require('./resolvers/Mutation');
-const AuthPayload = require('./resolvers/AuthPayload');
-const Subscription = require('./resolvers/Subscription');
-const Feed = require('./resolvers/Feed');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const resolvers = {
-  Query,
-  Mutation,
-  AuthPayload,
-  Subscription,
-  Feed
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context: req => ({
-    ...req,
-    db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://us1.prisma.sh/joe-friesen-2baea5/graphql-hackernews/dev',
-      secret: 'mysecret123',
-      debug: true
-    })
-  })
-})
-server.start(() => console.log('Server is running on http://localhost:4000'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
