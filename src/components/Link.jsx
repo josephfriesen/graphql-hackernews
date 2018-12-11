f4ca10202457d19fb0cf6f3b18c5fbe4de0e21e2
Joe Friesen <friesen.josephc@gmail.com>
2018-12-10 22:21:49 -0800

    React/Apollo tutorial complete.

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,11 +1,92 @@
 import React from 'react'
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      node {
+        id
+        link {
+          id
+          url
+          description
+          createdAt
+          postedBy {
+            id
+            name
+          }
+          votes {
+            id
+            user {
+              id
+            }
+          }
+        }
+        user {
+          id
+        }
+      }
+    }
+  }
+`
 
 export default class Link extends React.Component {
+
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN)
     return (
-      <div>
-        <div>
-          {this.props.link.description} ({this.props.link.url})
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{this.props.index + 1}.</span>
+          {authToken && (
+            <Mutation
+              mutation={VOTE_MUTATION}
+              variables={{ linkId: this.props.link.id }}
+              update={(store, { data: { vote } }) =>
+                this.props.updateStoreAfterVote(store, vote, this.props.link.id)
+              }
+            >
+              {voteMutation => (
+                <div className="ml1 gray f11 pointer" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {this.props.link.description} ({this.props.link.url})
+          </div>
+          <div className="f6 lh-copy-gray">
+            {this.props.link.votes.length} votes | by{' '}
+            {this.props.link.postedBy
+              ? this.props.link.postedBy.name
+              : 'Who the hell knows'}{' '}
+            {timeDifferenceForDate(this.props.link.createdAt)}
+          </div>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -7,20 +7,60 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
-const httpLink = createHttpLink({ // Connect ApolloClient with the GraphQL API running on port 4000
+// Connect ApolloClient with the GraphQL API running on port 4000, http-link handles queries and mutations
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// Apollo middleware that is invoked whenever ApolloClient sends a request to the server
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+// WebSocket-Link handles subscriptions
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connnectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 
